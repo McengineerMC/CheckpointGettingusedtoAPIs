f76feeb613bd201da00eec1beeeb847b05ab773b
Messaoudi <messaoudichedi54@yahoo.fr>
2020-08-26 14:52:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import UserList from "./UserList";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+function App() {
+ return (
+   
+  <div>
+ <h1>User List</h1>
+ <div className="list-container">
+   <UserList/>
+ </div>
+ </div>
+ )
+}
 export default App;
